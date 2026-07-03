8fa8cba04676acafc8fe385925607963de43f967
MSIC2 <msimmiandclean@gmail.com>
2026-07-03 14:43:26 +0200

    Update app/page.tsx - Add Stats and Services components

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,24 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import Stats from "@/components/Stats";
+import Services from "@/components/Services";
 
 export default function Home() {
 
-  return (
+return(
 
-    <>
+<>
 
-      <Navbar />
+<Navbar/>
 
-      <Hero />
+<Hero/>
 
-    </>
+<Stats/>
 
-  );
+<Services/>
+
+</>
+
+);
 
 }
